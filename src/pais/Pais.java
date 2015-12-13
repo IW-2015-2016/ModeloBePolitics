1b21ca737eb1afbf84d0a5e7e2093925ca8690a4
IGonjal <kaostias@gmail.com>
2015-12-13 13:55:35 +0100

    Politico
    Acabado Político y todas sus relaciones. Falta Casi todo el paquete
    Pais, todo el paquete evento, las relaciones entre paises....

--- src/pais/Pais.java
+++ src/pais/Pais.java
@@ -1,5 +1,9 @@
 package pais;
 
+import java.util.ArrayList;
+import politicos.Politico;
+
 public class Pais {
+	private ArrayList<Politico> politicos;
 
 }
--- src/pais/construcciones/Construccion.java
+++ src/pais/construcciones/Construccion.java
@@ -1,26 +1,47 @@
 package pais.construcciones;
 
 import pais.TipoRecurso;
+import politicos.Politico;
 
 public class Construccion {
 	private String nombre;
 	private TipoRecurso rec;
 	private int nivel;
 	private int produccion_hora;
+	private Politico politicoAlojado;
 	
 	private int coste[][];
 	
 	
 	private int multiplicadorProduccion=2;
 	private int multiplicadorCoste=3;
-	
+	/**
+	 * 
+	 * @param nombre
+	 * @param rec
+	 */
 	public Construccion(String nombre, TipoRecurso rec){
 		this.nombre = nombre;
 		this.rec = rec;
 		this.nivel = 1;
 		this.produccion_hora = 5;
-		
 	}
 	
+	public Construccion(String nombre, TipoRecurso rec, int multiplicadorProduccion, int multiplicadorCoste){
+		this.nombre = nombre;
+		this.rec = rec;
+		this.nivel = 1;
+		this.produccion_hora = 5;
+		this.multiplicadorProduccion = multiplicadorProduccion;
+		this.multiplicadorCoste = multiplicadorCoste;
+	}
+	
+	public void setPolitico(Politico newPolitico){
+		this.politicoAlojado = newPolitico;
+	}
+	
+	public Politico getPolitico(){
+		return this.politicoAlojado;
+	}
 	
 }
